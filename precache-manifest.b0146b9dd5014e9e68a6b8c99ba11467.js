5b371363d45d96f7f2f90d918447df415340c551
Firwing-fawn <59110834+Firwing-fawn@users.noreply.github.com>
2020-04-22 21:19:46 +0300

    Updates

---
+++ precache-manifest.b0146b9dd5014e9e68a6b8c99ba11467.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0636edc91f0911b483872ad7b67a80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a03d0fcdb0197fdb97e",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "614ac71469180c0b07a5",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4a03d0fcdb0197fdb97e",
+    "url": "/static/js/2.0fe0b2f7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.0fe0b2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614ac71469180c0b07a5",
+    "url": "/static/js/main.3e50306a.chunk.js"
+  },
+  {
+    "revision": "db9315e5999c411c74d4",
+    "url": "/static/js/runtime-main.824c3f63.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
